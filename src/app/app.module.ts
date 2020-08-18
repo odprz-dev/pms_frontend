3b958bce156d430e568f13288d0ec9275a3e7924
Oscar Daniel Perez <odprz.dev@gmail.com>
2020-08-18 12:43:45 -0500

    feat: se agregan modulos y componentes base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatListModule} from '@angular/material/list'
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    FlexLayoutModule,
+    MatListModule
+  ],
+  exports:[
+    MatButtonModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    FlexLayoutModule,
+    MatListModule
+
+  ]
+})
+export class MaterialModule { }
