2fdbb01da40fadbc8fc5f6675ded6409ce02b1c5
Oscar Daniel Perez <odprz.dev@gmail.com>
2020-08-18 19:10:50 -0500

    chore: altura de modal

--- src/app/modules/user/usuarios/usuarios.component.ts
+++ src/app/modules/user/usuarios/usuarios.component.ts
@@ -51,7 +51,7 @@ export class UsuariosComponent implements OnInit {
   openDialog(action:string, data: any){
     data.action = action;
     console.log(data);
-    const dialogRef = this.dialog.open(UserModalComponent, {width: '640px', height: '70%' ,   scrollStrategy: this.overlay.scrollStrategies.reposition(), disableClose: true, data: data});
+    const dialogRef = this.dialog.open(UserModalComponent, {width: '640px', height: '43%' ,   scrollStrategy: this.overlay.scrollStrategies.reposition(), disableClose: true, data: data});
     dialogRef.afterClosed()
       .subscribe(result =>{
         console.log('Devolucion del child: ',result)
